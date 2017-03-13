cf5ca5596b9a2b446cf6e8cf702dae876fbb0229
Huynh Vu <huynhvu@Huynhs-iMac.local>
2017-03-12 21:47:53 -0500

    updated raise rates

--- raise_starbucks.R
+++ raise_starbucks.R
@@ -13,12 +13,8 @@ best_rate <- apply(head(raise_data, 1), 1, paste)
 # trim variable obtain just the percentage
 best_rate <- as.numeric(gsub("([0-9]+).*$", "\\1", substring(best_rate, 19, 22)))
 
-# create a txt document with raise rates
-write.table(Sys.time(), "data/raise_starbucks.txt",
-            sep = "", row.names = FALSE, quote = FALSE)
-
 # writes the table of best rate info
-write.table(best_rate, "data/raise_starbucks.txt",
+write.table(best_rate, "data/raise_starbucks.csv",
             sep = "", row.names = FALSE, quote = FALSE, append = TRUE)
 
 # add other favorite credit cards
