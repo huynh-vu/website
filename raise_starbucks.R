c9bc169c7563d9cedf71307f07e0ea3f9a236a5b
Huynh Vu <huynhvu@Huynhs-iMac.local>
2017-03-12 21:43:24 -0500

    updated raise rates

--- raise_starbucks.R
+++ raise_starbucks.R
@@ -7,10 +7,11 @@ download.file("https://www.raise.com/buy-starbucks-coffee-gift-cards",destfile="
 # read file 
 raise_data <- read.csv("data/raise.csv",header=FALSE, skip = 379)
 
-# print file
-str(raise_data)
+# convert to character
+best_rate <- apply(head(raise_data, 1), 1, paste)
 
-best_rate <- head(raise_data)
+# trim variable obtain just the percentage
+as.numeric(gsub("([0-9]+).*$", "\\1", substring(best_rate, 19, 22)))
 
 # create a txt document with raise rates
 write.table(Sys.time(), "data/raise_starbucks.txt",
